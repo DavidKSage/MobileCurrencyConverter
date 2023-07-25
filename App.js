9bda949c1e7857fa4e0da1e6ddfdbb3cc347c9e2
David K. Sage <dksage@icloud.com>
2023-07-25 17:39:15 -0400

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.header}>
+        <View>
+          <Text style={{ fontSize: 36 }}>Currency Converter</Text>
+        </View>
+      </SafeAreaView>
+      <View style={{ alignItems: "center", height: 50, paddingTop: 5 }}>
+        <Text>Convert from:</Text>
+      </View>
+      <View style={{ alignItems: "center" }}>
+        <Text>Conver to:</Text>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    height: 150,
+    backgroundColor: "lavender",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
