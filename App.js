7572c98f816dcfb3164148a033891afd658cfa51
David K. Sage <dksage@icloud.com>
2023-07-30 18:46:37 -0400

    Added picker component

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
+import { currOptions } from "./Options";
+import Picker from "@ouroboros/react-native-picker";
 
 export default function App() {
+  const [fromCur, setFromCur] = useState("USD");
+  const [toCur, setToCur] = useState("EUR");
+
   return (
     <>
       <SafeAreaView style={styles.header}>
@@ -9,20 +14,42 @@ export default function App() {
           <Text style={{ fontSize: 36 }}>Currency Converter</Text>
         </View>
       </SafeAreaView>
-      <View style={{ alignItems: "center", height: 50, paddingTop: 5 }}>
-        <Text styles={{ fontSize: 48 }}>Convert from:</Text>
-      </View>
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <Text>From</Text>
-      </View>
-      <View style={{ alignItems: "center", height: 50 }}>
-        <Text>Conver to:</Text>
-      </View>
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <Text>From:</Text>
-      </View>
-      <View style={{ flex: 1, alignItems: "center" }}>
-        <Text style={{ fontSize: 30 }}>1 USD = ______ EUR</Text>
+      <View
+        style={{
+          alignItems: "center",
+          paddingTop: 5,
+          flex: 1,
+        }}
+      >
+        <Text style={{ fontSize: 24 }}>Convert from:</Text>
+        <Picker
+          onChanged={setFromCur}
+          options={currOptions}
+          style={{
+            borderWidth: 1,
+            borderColor: "#a7a7a7",
+            borderRadius: 5,
+            marginBottom: 5,
+            padding: 5,
+            width: 300,
+            textAlign: "center",
+          }}
+          value={fromCur}
+        />
+        <Text style={{ fontSize: 24, marginTop: 20 }}>Convert to:</Text>
+        <Picker
+          onChanged={setToCur}
+          options={currOptions}
+          style={{
+            borderWidth: 1,
+            borderColor: "#a7a7a7",
+            borderRadius: 5,
+            marginBottom: 5,
+            padding: 5,
+            width: 300,
+          }}
+          value={toCur}
+        />
       </View>
     </>
   );
---
+++ Options.js
@@ -0,0 +1,552 @@
+export const currOptions = [
+  {
+    value: "ALL",
+    text: "Albania Lek",
+    symbol: "L",
+  },
+  {
+    value: "AFN",
+    text: "Afghanistan Afghani",
+    symbol: "؋",
+  },
+  {
+    text: "Argentina Peso",
+    value: "ARS",
+    symbol: "$",
+  },
+  {
+    text: "Aruba Guilder",
+    value: "AWG",
+    symbol: "ƒ.",
+  },
+  {
+    text: "Australia Dollar",
+    value: "AUD",
+    symbol: "$",
+  },
+  {
+    text: "Azerbaijan New Manat",
+    value: "AZN",
+    symbol: "₼",
+  },
+  {
+    text: "Bahamas Dollar",
+    value: "BSD",
+    symbol: "$",
+  },
+  {
+    text: "Barbados Dollar",
+    value: "BBD",
+    symbol: "$",
+  },
+  {
+    text: "Belarus Ruble",
+    value: "BYR",
+    symbol: "Br",
+  },
+  {
+    text: "Belize Dollar",
+    value: "BZD",
+    symbol: "BZ$",
+  },
+  {
+    text: "Bermuda Dollar",
+    value: "BMD",
+    symbol: "$",
+  },
+  {
+    text: "Bolivia Boliviano",
+    value: "BOB",
+    symbol: "$b",
+  },
+  {
+    text: "Bosnia and Herzegovina Convertible Marka",
+    value: "BAM",
+    symbol: "KM",
+  },
+  {
+    text: "Botswana Pula",
+    value: "BWP",
+    symbol: "P",
+  },
+  {
+    text: "Bulgaria Lev",
+    value: "BGN",
+    symbol: "лв",
+  },
+  {
+    text: "Brazil Real",
+    value: "BRL",
+    symbol: "R$",
+  },
+  {
+    text: "Brunei Darussalam Dollar",
+    value: "BND",
+    symbol: "$",
+  },
+  {
+    text: "Cambodia Riel",
+    value: "KHR",
+    symbol: "៛",
+  },
+  {
+    text: "Canada Dollar",
+    value: "CAD",
+    symbol: "$",
+  },
+  {
+    text: "Cayman Islands Dollar",
+    value: "KYD",
+    symbol: "$",
+  },
+  {
+    text: "Chile Peso",
+    value: "CLP",
+    symbol: "$",
+  },
+  {
+    text: "China Yuan Renminbi",
+    value: "CNY",
+    symbol: "¥",
+  },
+  {
+    text: "Colombia Peso",
+    value: "COP",
+    symbol: "$",
+  },
+  {
+    text: "Costa Rica Colon",
+    value: "CRC",
+    symbol: "₡",
+  },
+  {
+    text: "Croatia Kuna",
+    value: "HRK",
+    symbol: "kn",
+  },
+  {
+    text: "Cuba Peso",
+    value: "CUP",
+    symbol: "₱",
+  },
+  {
+    text: "Czech Republic Koruna",
+    value: "CZK",
+    symbol: "Kč",
+  },
+  {
+    text: "Denmark Krone",
+    value: "DKK",
+    symbol: "kr",
+  },
+  {
+    text: "Dominican Republic Peso",
+    value: "DOP",
+    symbol: "RD$",
+  },
+  {
+    text: "East Caribbean Dollar",
+    value: "XCD",
+    symbol: "$",
+  },
+  {
+    text: "Egypt Pound",
+    value: "EGP",
+    symbol: "£",
+  },
+  {
+    text: "El Salvador Colon",
+    value: "SVC",
+    symbol: "$",
+  },
+  {
+    text: "Euro",
+    value: "EUR",
+    symbol: "€",
+  },
+  {
+    text: "Falkland Islands (Malvinas) Pound",
+    value: "FKP",
+    symbol: "£",
+  },
+  {
+    text: "Fiji Dollar",
+    value: "FJD",
+    symbol: "$",
+  },
+  {
+    text: "Ghana Cedis",
+    value: "GHC",
+    symbol: "¢",
+  },
+  {
+    text: "Gibraltar Pound",
+    value: "GIP",
+    symbol: "£",
+  },
+  {
+    text: "Guatemala Quetzal",
+    value: "GTQ",
+    symbol: "Q",
+  },
+  {
+    text: "Guernsey Pound",
+    value: "GGP",
+    symbol: "£",
+  },
+  {
+    text: "Guyana Dollar",
+    value: "GYD",
+    symbol: "$",
+  },
+  {
+    text: "Honduras Lempira",
+    value: "HNL",
+    symbol: "L",
+  },
+  {
+    text: "Hong Kong Dollar",
+    value: "HKD",
+    symbol: "L",
+  },
+  {
+    text: "Hungary Forint",
+    value: "HUF",
+    symbol: "Ft",
+  },
+  {
+    text: "Iceland Krona",
+    value: "ISK",
+    symbol: "kr",
+  },
+  {
+    text: "India Rupee",
+    value: "INR",
+    symbol: "₹",
+  },
+  {
+    text: "Indonesia Rupiah",
+    value: "IDR",
+    symbol: "Rp",
+  },
+  {
+    text: "Iran Rial",
+    value: "IRR",
+    symbol: "﷼",
+  },
+  {
+    text: "Isle of Man Pound",
+    value: "IMP",
+    symbol: "£",
+  },
+  {
+    text: "Israel Shekel",
+    value: "ILS",
+    symbol: "₪",
+  },
+  {
+    text: "Jamaica Dollar",
+    value: "JMD",
+    symbol: "J$",
+  },
+  {
+    text: "Japan Yen",
+    value: "JPY",
+    symbol: "¥",
+  },
+  {
+    text: "Jersey Pound",
+    value: "JEP",
+    symbol: "£",
+  },
+  {
+    text: "Kazakhstan Tenge",
+    value: "KZT",
+    symbol: "лв",
+  },
+  {
+    text: "Korea (North) Won",
+    value: "KPW",
+    symbol: "₩",
+  },
+  {
+    text: "Korea (South) Won",
+    value: "KRW",
+    symbol: "₩",
+  },
+  {
+    text: "Kyrgyzstan Som",
+    value: "KGS",
+    symbol: "лв",
+  },
+  {
+    text: "Laos Kip",
+    value: "LAK",
+    symbol: "₭",
+  },
+  {
+    text: "Lebanon Pound",
+    value: "LBP",
+    symbol: "£",
+  },
+  {
+    text: "Liberia Dollar",
+    value: "LRD",
+    symbol: "$",
+  },
+  {
+    text: "Lithuania Litas",
+    value: "LTL",
+    symbol: "Lt",
+  },
+  {
+    text: "Macedonia Denar",
+    value: "MKD",
+    symbol: "ден",
+  },
+  {
+    text: "Malaysia Ringgit",
+    value: "MYR",
+    symbol: "RM",
+  },
+  {
+    text: "Mauritius Rupee",
+    value: "MUR",
+    symbol: "₨",
+  },
+  {
+    text: "Mexico Peso",
+    value: "MXN",
+    symbol: "$",
+  },
+  {
+    text: "Mongolia Tughrik",
+    value: "MNT",
+    symbol: "₮",
+  },
+  {
+    text: "Mozambique Metical",
+    value: "MZN",
+    symbol: "MT",
+  },
+  {
+    text: "Namibia Dollar",
+    value: "NAD",
+    symbol: "$",
+  },
+  {
+    text: "Nepal Rupee",
+    value: "NPR",
+    symbol: "₨",
+  },
+  {
+    text: "Netherlands Antilles Guilder",
+    value: "ANG",
+    symbol: "ƒ",
+  },
+  {
+    text: "New Zealand Dollar",
+    value: "NZD",
+    symbol: "$",
+  },
+  {
+    text: "Nicaragua Cordoba",
+    value: "NIO",
+    symbol: "C$",
+  },
+  {
+    text: "Nigeria Naira",
+    value: "NGN",
+    symbol: "₦",
+  },
+  {
+    text: "Norway Krone",
+    value: "NOK",
+    symbol: "kr",
+  },
+  {
+    text: "Oman Rial",
+    value: "OMR",
+    symbol: "﷼",
+  },
+  {
+    text: "Pakistan Rupee",
+    value: "PKR",
+    symbol: "₨",
+  },
+  {
+    text: "Panama Balboa",
+    value: "PAB",
+    symbol: "B/.",
+  },
+  {
+    text: "Paraguay Guarani",
+    value: "PYG",
+    symbol: "Gs",
+  },
+  {
+    text: "Peru Nuevo Sol",
+    value: "PEN",
+    symbol: "S/.",
+  },
+  {
+    text: "Philippines Peso",
+    value: "PHP",
+    symbol: "₱",
+  },
+  {
+    text: "Poland Zloty",
+    value: "PLN",
+    symbol: "zł",
+  },
+  {
+    text: "Qatar Riyal",
+    value: "QAR",
+    symbol: "﷼",
+  },
+  {
+    text: "Romania New Leu",
+    value: "RON",
+    symbol: "lei",
+  },
+  {
+    text: "Russia Ruble",
+    value: "RUB",
+    symbol: "₽",
+  },
+  {
+    text: "Saint Helena Pound",
+    value: "SHP",
+    symbol: "£",
+  },
+  {
+    text: "Saudi Arabia Riyal",
+    value: "SAR",
+    symbol: "﷼",
+  },
+  {
+    text: "Serbia Dinar",
+    value: "RSD",
+    symbol: "Дин",
+  },
+  {
+    text: "Seychelles Rupee",
+    value: "SCR",
+    symbol: "₨",
+  },
+  {
+    text: "Singapore Dollar",
+    value: "SGD",
+    symbol: "$",
+  },
+  {
+    text: "Solomon Islands Dollar",
+    value: "SBD",
+    symbol: "$",
+  },
+  {
+    text: "Somalia Shilling",
+    value: "SOS",
+    symbol: "S",
+  },
+  {
+    text: "South Africa Rand",
+    value: "ZAR",
+    symbol: "R",
+  },
+  {
+    text: "Sri Lanka Rupee",
+    value: "LKR",
+    symbol: "₨",
+  },
+  {
+    text: "Sweden Krona",
+    value: "SEK",
+    symbol: "kr",
+  },
+  {
+    text: "Switzerland Franc",
+    value: "CHF",
+    symbol: "CHF",
+  },
+  {
+    text: "Suriname Dollar",
+    value: "SRD",
+    symbol: "$",
+  },
+  {
+    text: "Syria Pound",
+    value: "SYP",
+    symbol: "£",
+  },
+  {
+    text: "Taiwan New Dollar",
+    value: "TWD",
+    symbol: "NT$",
+  },
+  {
+    text: "Thailand Baht",
+    value: "THB",
+    symbol: "฿",
+  },
+  {
+    text: "Trinidad and Tobago Dollar",
+    value: "TTD",
+    symbol: "TT$",
+  },
+  {
+    text: "Turkey Lira",
+    value: "TRY",
+    symbol: "₺",
+  },
+  {
+    text: "Tuvalu Dollar",
+    value: "TVD",
+    symbol: "$",
+  },
+  {
+    text: "Ukraine Hryvna",
+    value: "UAH",
+    symbol: "₴",
+  },
+  {
+    text: "United Kingdom Pound",
+    value: "GBP",
+    symbol: "£",
+  },
+  {
+    text: "United States Dollar",
+    value: "USD",
+    symbol: "$",
+  },
+  {
+    text: "Uruguay Peso",
+    value: "UYU",
+    symbol: "$U",
+  },
+  {
+    text: "Uzbekistan Som",
+    value: "UZS",
+    symbol: "лв",
+  },
+  {
+    text: "Venezuela Bolivar",
+    value: "VEF",
+    symbol: "Bs",
+  },
+  {
+    text: "Viet Nam Dong",
+    value: "VND",
+    symbol: "₫",
+  },
+  {
+    text: "Yemen Rial",
+    value: "YER",
+    symbol: "﷼",
+  },
+  {
+    text: "Zimbabwe Dollar",
+    value: "ZWD",
+    symbol: "Z$",
+  },
+];
